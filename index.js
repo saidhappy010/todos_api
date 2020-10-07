556afcecf4236c3db34e8460a3cad9d8f76eacc2
kouzzi010 <kouzzibrhm@gmail.com>
2020-10-07 22:12:33 +0100

    On a créé index route

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require ('express'),
-    app = express();
+    app = express(),
+    todoRoutes = require('./routes/todos');
 const port = 3000;
+
+app.use('/api/todos', todoRoutes);
 
 app.get('/', function(req, res){
     res.send('HI THERE FROM EXPRESS');
